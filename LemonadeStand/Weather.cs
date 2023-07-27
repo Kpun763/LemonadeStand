74a89cd3cf9f60262e4e88deb806907188c9e0d6
Kyle Washington <kmw1993@msn.com>
2023-07-27 13:52:38 -0400

    Uploading the created weather class with forecase, temperature, and condition methods

---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    internal class Weather
+    {
+
+        public int Temperature { get; set; }
+        public string Condition { get; set; }
+
+        public void GenereateForecase()
+        {
+            Random random = new Random();
+            Temperature = random.Next(50, 90); //This is random temperature between 50 degrees Fahreheit to 89 degrees Fahrenheit
+
+            // Used to select a random weather conditon
+            string[] weatherCondition = { "Sunny", "Partly Cloudy", "Scattered Thunderstorms" };
+            int randomIndex = random.Next(weatherCondition.Length);
+            Condition = weatherCondition[randomIndex];
+        }
+
+        public void DisplayForecast()
+        {
+            Console.WriteLine($"The Forecast for today is:");
+            Console.WriteLine($"Temperature: {Temperature} {(char)176}F");
+            Console.WriteLine($"Condition: {Condition}");
+        }
+        
+
+    }
+    
+}
