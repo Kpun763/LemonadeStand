222fbdcc978f9392954b4c7fab61e6a593726021
Kyle Washington <kmw1993@msn.com>
2023-07-31 12:45:55 -0400

    Adding the addition of a standard recipe in my LemonadeRecipe  class.

--- LemonadeStand/LemonadeRecipe.cs
+++ LemonadeStand/LemonadeRecipe.cs
@@ -8,5 +8,17 @@ namespace LemonadeStand
 {
     internal class LemonadeRecipe
     {
+        public int LemonsPerPitcher { get; set; }
+        public int SugarCubesPerPitcher { get; set; }
+        public int IceCubesPerPitcher { get; set; }
+        public int CupsPerPitcher { get; set; }
+        public LemonadeRecipe()
+        {
+            LemonsPerPitcher = 1;
+            SugarCubesPerPitcher = 3;
+            IceCubesPerPitcher = 3;
+            CupsPerPitcher = 5;
+        }
+
     }
 }
